04b705f6f25da76ee862800ca92a5c1a4fa70abd
Sergio Toro <yiti007@gmail.com>
2015-02-19 18:36:09 +0100

    Add fibonacci tests

--- src/Storo/Tests/FibonacciTest.php
+++ src/Storo/Tests/FibonacciTest.php
@@ -23,7 +23,37 @@ class FibonacciTest extends \PHPUnit_Framework_TestCase
     {
         $result = $fibonacci->recursive(5);
 
-        $this->assertEquals($result, 8);
+        $this->assertEquals(5, $result);
+    }
+
+    /**
+     * @depends testFibonacciInstance
+     */
+    public function testRecursiveResult2(Fibonacci $fibonacci)
+    {
+        $result = $fibonacci->recursive(8);
+
+        $this->assertEquals(21, $result);
+    }
+
+    /**
+     * @depends testFibonacciInstance
+     */
+    public function testRecursiveResult3(Fibonacci $fibonacci)
+    {
+        $result = $fibonacci->recursive(10);
+
+        $this->assertEquals(55, $result);
+    }
+
+    /**
+     * @depends testFibonacciInstance
+     */
+    public function testRecursiveResult4(Fibonacci $fibonacci)
+    {
+        $result = $fibonacci->recursive(14);
+
+        $this->assertEquals(377, $result);
     }
 
     /**
@@ -33,7 +63,37 @@ class FibonacciTest extends \PHPUnit_Framework_TestCase
     {
         $result = $fibonacci->iterative(5);
 
-        $this->assertEquals($result, 8);
+        $this->assertEquals(5, $result);
+    }
+
+    /**
+     * @depends testFibonacciInstance
+     */
+    public function testIterativeResult2(Fibonacci $fibonacci)
+    {
+        $result = $fibonacci->iterative(8);
+
+        $this->assertEquals(21, $result);
+    }
+
+    /**
+     * @depends testFibonacciInstance
+     */
+    public function testIterativeResult3(Fibonacci $fibonacci)
+    {
+        $result = $fibonacci->iterative(10);
+
+        $this->assertEquals(55, $result);
+    }
+
+    /**
+     * @depends testFibonacciInstance
+     */
+    public function testIterativeResult4(Fibonacci $fibonacci)
+    {
+        $result = $fibonacci->iterative(14);
+
+        $this->assertEquals(377, $result);
     }
 
 }
