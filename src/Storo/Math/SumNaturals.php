b74fcc4b86e7bdbb2c6deec2b5e6642ecbe54210
Sergio Toro <yiti007@gmail.com>
2015-02-20 12:39:59 +0100

    Add efficient method to get the sum of all natural numbers below $limit that are multiples of $a or $b in a efficient way.

--- src/Storo/Math/SumNaturals.php
+++ src/Storo/Math/SumNaturals.php
@@ -5,10 +5,11 @@ namespace Storo\Math;
 class SumNaturals
 {
     /**
-     * Returns the sum of all natural numbers below $limit that are multiples of $a or 5$b.
-     * @param  integer $a      First number to check
-     * @param  integer $b      Second number to check
-     * @param  integer $limit  Limit number
+     * Returns the sum of all natural numbers below $limit that are
+     * multiples of $a or $b, not in a efficient way.
+     * @param  integer $a
+     * @param  integer $b
+     * @param  integer $limit
      * @return integer
      */
     public function sumIfMultipleBelow($a, $b, $limit = 1000)
@@ -22,6 +23,70 @@ class SumNaturals
             }
         }
 
-        return $total;
+        return (integer) $total;
+    }
+
+    /**
+     * Returns the sum of all natural numbers below $limit that are
+     * multiples of $a or $b in a efficient way.
+     * @param  integer $a
+     * @param  integer $b
+     * @param  integer $limit
+     * @return integer
+     */
+    public function getSumOfMultiplesOfAOrB($a, $b, $limit = 1000)
+    {
+        if ($a%$b==0 || $b%$a==0) {
+            return $this->sumMul($limit, ($a<$b ? $a : $b));
+        }
+        else {
+            return (integer) $this->sumMul($limit, $a) +
+                $this->sumMul($limit, $b) -
+                $this->sumMul($limit, $this->mcm($a,$b))
+            ;
+        }
+    }
+
+
+    /**
+     * Returns the sum of all natural numbers below $limit that are
+     * multiples of $number
+     * @param  integer $limit
+     * @param  integer $number
+     * @return integer
+     */
+    private function sumMul($limit, $number)
+    {
+        $greatestMultiple = $limit-1;
+        while ($greatestMultiple%$number != 0) {
+            $greatestMultiple--;
+        }
+        $last = $greatestMultiple / $number;
+        return (integer) $number * $last * ($last + 1) / 2;
+    }
+
+    /**
+     * Returns the minimum common divisor of two numbers
+     * @param  integer $a
+     * @param  integer $b
+     * @return integer
+     */
+    private function mcd($a, $b) {
+        while (($a % $b) != 0) {
+            $c = $b;
+            $b = $a % $b;
+            $a = $c;
+        }
+        return (integer) $b;
+    }
+
+    /**
+     * Returns the minimum common multiple of two numbers
+     * @param  integer $a
+     * @param  integer $b
+     * @return integer
+     */
+    private function mcm($a, $b) {
+        return (integer) ($a * $b) / $this->mcd($a,$b);
     }
 }
